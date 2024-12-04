e010136ecdefc2bb479d372f6ad844dd452ac8f3
g-larios <82904321+g-larios@users.noreply.github.com>
2024-12-04 10:57:24 -0600

    Higgs potential in 4d

---
+++ Potential_generator/pot_Higgs_4param.py
@@ -0,0 +1,14 @@
+
+import tensorflow as tf
+import numpy as np
+
+dim = 4
+
+def V(x):
+    """
+    V = (-1 + x1 ** 2 + x2 ** 2) ** 2 + x3 ** 2 + x4 ** 2
+    """
+
+    x1, x2, x3, x4 = tf.split(x, 4, axis=1)
+
+    return (-1 + x1 ** 2 + x2 ** 2) ** 2 + x3 ** 2 + x4 ** 2
